eb2a2f2dd7eb92704f7c003b1dfd86f33a17ca66
manoharlikhit <gouthamvenkat2002@gmail.com>
2024-12-14 11:29:46 +0530

    add CKEditor 5 dependencies and enhance styling for editor components

--- src/App.jsx
+++ src/App.jsx
@@ -1,126 +1,311 @@
-import { useRef } from "react";
-import { Editor } from "@tinymce/tinymce-react";
+import { useState, useEffect, useRef, useMemo } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Alignment,
+  AutoLink,
+  Autosave,
+  BlockQuote,
+  Bold,
+  Bookmark,
+  Code,
+  CodeBlock,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  Paragraph,
+  RemoveFormat,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  Underline,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
+
 import "./App.css";
 
-export default function App() {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
+const LICENSE_KEY =
+  "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3MzU0MzAzOTksImp0aSI6ImJiODU1NGQ5LTgxMGQtNDQ1Ny05YzIxLTA4NmIwZjY1MjVjMCIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImYyMmI2ZmQ5In0.9UJEsAyLbheZJXnoxTMbjEwha6p6NgsfHusMshNZ3fCpBtCIvv2x2154wyS5vvQ77cPmX3pIlgZkRni9OCd7Tg";
 
-  return (
-    <>
-      <Editor
-        apiKey="nsmf0p63cr6tsby6efmy5wwme5svcgsq8eeulvakknhj9h91"
-        onInit={(_evt, editor) => (editorRef.current = editor)}
-        init={{
-          height: "90vh", // Set to full viewport height
-          width: "80vw", // Set to full viewport width
-          menubar: false,
-          branding: false,
-          plugins: [
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "media",
-            "table",
-            "code",
-            "help",
-            "fontsize",
-            "fontfamily",
-            "wordcount",
+export default function App() {
+  const editorContainerRef = useRef(null);
+  const editorRef = useRef(null);
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+
+  useEffect(() => {
+    setIsLayoutReady(true);
+
+    return () => setIsLayoutReady(false);
+  }, []);
+
+  const { editorConfig } = useMemo(() => {
+    if (!isLayoutReady) {
+      return {};
+    }
+
+    return {
+      editorConfig: {
+        toolbar: {
+          items: [
+            "findAndReplace",
+            "|",
+            "heading",
+            "style",
+            "|",
+            "fontSize",
+            "fontFamily",
+            "fontColor",
+            "fontBackgroundColor",
+            "|",
+            "bold",
+            "italic",
             "underline",
             "strikethrough",
-            "lineheight",
-            "superscript",
             "subscript",
-            "codesample",
-            "table",
-            "hr",
-            "spellchecker",
-            "speech",
+            "superscript",
+            "code",
+            "removeFormat",
+            "|",
+            "specialCharacters",
+            "horizontalLine",
+            "link",
+            "bookmark",
+            "insertTable",
+            "highlight",
+            "blockQuote",
+            "codeBlock",
+            "|",
+            "alignment",
+            "|",
+            "outdent",
+            "indent",
           ],
-          toolbar:
-            "undo redo | blocks | bold italic underline strikethrough superscript subscript forecolor | fontfamily fontsize lineheight | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | charmap searchreplace | table hr | removeformat | link image media |code fullscreen preview codesample spellchecker | speech | help",
-          toolbar_mode: "sliding",
-          font_family_formats:
-            "Poppins=Poppins, sans-serif;" +
-            "Andale Mono=andale mono,times;" +
-            "Arial=arial,helvetica,sans-serif;" +
-            "Arial Black=arial black,avant garde;" +
-            "Book Antiqua=book antiqua,palatino;" +
-            "Comic Sans MS=comic sans ms,sans-serif;" +
-            "Courier New=courier new,courier,monospace;" +
-            "Georgia=georgia,palatino,serif;" +
-            "Helvetica=helvetica;" +
-            "Impact=impact,chicago;" +
-            "Symbol=symbol;" +
-            "Tahoma=tahoma,arial,helvetica,sans-serif;" +
-            "Terminal=terminal,monaco;" +
-            "Times New Roman=times new roman,times,serif;" +
-            "Trebuchet MS=trebuchet ms,geneva;" +
-            "Verdana=verdana,geneva,sans-serif;" +
-            "Webdings=webdings;" +
-            "Wingdings=wingdings,zapf dingbats;",
-          content_style:
-            "@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');" +
-            "body { font-family:Poppins, sans-serif; font-size:14px; margin:0; padding:0; }",
-          spellchecker_dialog: true,
-          spellchecker_whitelist: ["TinyMCE", "JavaScript"],
-          speech_recognition: true,
-          file_picker_callback: async (callback, value, meta) => {
-            if (meta.filetype === "image") {
-              const input = document.createElement("input");
-              input.setAttribute("type", "file");
-              input.setAttribute("accept", "image/*");
-              input.onchange = async function () {
-                const file = this.files[0];
-                if (file) {
-                  const formData = new FormData();
-                  formData.append("file", file);
-                  try {
-                    const response = await fetch(
-                      "https://demo.mobilemasala.com/api/s3/upload?Content-Type=multipart/form-data",
-                      { method: "POST", body: formData }
-                    );
-                    if (!response.ok) throw new Error("Failed to upload file");
-                    const responseText = await response.json();
-                    callback(responseText.FILE_URL, { alt: file.name });
-                  } catch (error) {
-                    console.error("Error uploading file:", error);
-                  }
-                }
-              };
-              input.click();
-            } else if (meta.filetype === "media") {
-              const input = document.createElement("input");
-              input.setAttribute("type", "file");
-              input.setAttribute("accept", "video/*");
-              input.onchange = function () {
-                const file = this.files[0];
-                if (file) {
-                  const fileUrl = URL.createObjectURL(file);
-                  callback(fileUrl, { alt: file.name });
-                }
-              };
-              input.click();
-            }
+          shouldNotGroupWhenFull: true,
+        },
+        plugins: [
+          Alignment,
+          AutoLink,
+          Autosave,
+          BlockQuote,
+          Bold,
+          Bookmark,
+          Code,
+          CodeBlock,
+          Essentials,
+          FindAndReplace,
+          FontBackgroundColor,
+          FontColor,
+          FontFamily,
+          FontSize,
+          GeneralHtmlSupport,
+          Heading,
+          Highlight,
+          HorizontalLine,
+          Indent,
+          IndentBlock,
+          Italic,
+          Link,
+          Paragraph,
+          RemoveFormat,
+          SpecialCharacters,
+          SpecialCharactersArrows,
+          SpecialCharactersCurrency,
+          SpecialCharactersEssentials,
+          SpecialCharactersLatin,
+          SpecialCharactersMathematical,
+          SpecialCharactersText,
+          Strikethrough,
+          Style,
+          Subscript,
+          Superscript,
+          Table,
+          TableCaption,
+          TableCellProperties,
+          TableColumnResize,
+          TableProperties,
+          TableToolbar,
+          Underline,
+        ],
+        fontFamily: {
+          supportAllValues: true,
+        },
+        fontSize: {
+          options: [10, 12, 14, "default", 18, 20, 22],
+          supportAllValues: true,
+        },
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+            {
+              model: "heading4",
+              view: "h4",
+              title: "Heading 4",
+              class: "ck-heading_heading4",
+            },
+            {
+              model: "heading5",
+              view: "h5",
+              title: "Heading 5",
+              class: "ck-heading_heading5",
+            },
+            {
+              model: "heading6",
+              view: "h6",
+              title: "Heading 6",
+              class: "ck-heading_heading6",
+            },
+          ],
+        },
+        htmlSupport: {
+          allow: [
+            {
+              name: /^.*$/,
+              styles: true,
+              attributes: true,
+              classes: true,
+            },
+          ],
+        },
+        initialData:
+          '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
+        licenseKey: LICENSE_KEY,
+        link: {
+          addTargetToExternalLinks: true,
+          defaultProtocol: "https://",
+          decorators: {
+            toggleDownloadable: {
+              mode: "manual",
+              label: "Downloadable",
+              attributes: {
+                download: "file",
+              },
+            },
           },
-        }}
-      />
-      {/* <button onClick={log}>Log editor content</button> */}
-    </>
+        },
+        placeholder: "Type or paste your content here!",
+        style: {
+          definitions: [
+            {
+              name: "Article category",
+              element: "h3",
+              classes: ["category"],
+            },
+            {
+              name: "Title",
+              element: "h2",
+              classes: ["document-title"],
+            },
+            {
+              name: "Subtitle",
+              element: "h3",
+              classes: ["document-subtitle"],
+            },
+            {
+              name: "Info box",
+              element: "p",
+              classes: ["info-box"],
+            },
+            {
+              name: "Side quote",
+              element: "blockquote",
+              classes: ["side-quote"],
+            },
+            {
+              name: "Marker",
+              element: "span",
+              classes: ["marker"],
+            },
+            {
+              name: "Spoiler",
+              element: "span",
+              classes: ["spoiler"],
+            },
+            {
+              name: "Code (dark)",
+              element: "pre",
+              classes: ["fancy-code", "fancy-code-dark"],
+            },
+            {
+              name: "Code (bright)",
+              element: "pre",
+              classes: ["fancy-code", "fancy-code-bright"],
+            },
+          ],
+        },
+        table: {
+          contentToolbar: [
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "tableProperties",
+            "tableCellProperties",
+          ],
+        },
+      },
+    };
+  }, [isLayoutReady]);
+
+  return (
+    <div className="main-container">
+      <div
+        className="editor-container editor-container_classic-editor editor-container_include-style"
+        ref={editorContainerRef}>
+        <div className="editor-container__editor">
+          <div ref={editorRef}>
+            {editorConfig && (
+              <CKEditor editor={ClassicEditor} config={editorConfig} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
